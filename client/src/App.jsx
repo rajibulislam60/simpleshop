1cf3d77501e9acac6b44fd8d2e287ce2ee1c1dd8
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-09-11 21:22:41 +0600

    slick slider add done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div className="bg-green-500">Start Project</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/Bannar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Slider from "react-slick";
+import bannar from "../images/bannar.webp";
+import bannar1 from "../images/bannar1.webp";
+
+const data = [
+  {
+    id: 1,
+    image: bannar1,
+    name: "offer the product",
+  },
+  {
+    id: 2,
+    image: bannar,
+    name: "offer the product",
+  },
+];
+
+const Bannar = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+  };
+  return (
+    <div className="w-100%">
+      <Slider {...settings}>
+        {data.map((item) => (
+          <div key={item.id} className="w-full h-[400px]">
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default Bannar;
