9b57b2b9ad1b6ca384f27bcef3381439f1345f48
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-09-22 18:04:47 +0600

    container add

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Container from "./Container";
 
 const Search = ({ onSearch }) => {
   const [query, setQuery] = useState("");
@@ -10,18 +11,22 @@ const Search = ({ onSearch }) => {
   };
 
   return (
-    <div className="py-3 px-2 flex justify-between items-center">
-      <div className="flex gap-8 items-center">
-        <h1 className="text-2xl font-bold">SimpleShop</h1>
-        <h4 className="text-md font-normal">Brends</h4>
-      </div>
-      <input
-        type="text"
-        placeholder="Search products ....."
-        value={query}
-        onChange={handleSearch}
-        className="w-[450px] px-4 py-2 border border-gray-500 rounded-full"
-      />
+    <div>
+      <Container>
+        <div className="py-3 px-2 flex justify-between items-center">
+          <div className="flex gap-8 items-center">
+            <h1 className="text-2xl font-bold">SimpleShop</h1>
+            <h4 className="text-md font-normal">Brends</h4>
+          </div>
+          <input
+            type="text"
+            placeholder="Search products ....."
+            value={query}
+            onChange={handleSearch}
+            className="w-[450px] px-4 py-2 border border-gray-500 rounded-full"
+          />
+        </div>
+      </Container>
     </div>
   );
 };
