ff7c029a600d4235c32d46c637f2907b72a2d38b
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-09-26 23:28:07 +0600

    single product show done

--- client/src/components/data.jsx
+++ client/src/components/data.jsx
@@ -4,10 +4,38 @@ import serum from "../images/serum.png";
 import medicien from "../images/medicien.jpg";
 
 const data = [
-  { id: 1, name: "cream", image: cream, price: "599" },
-  { id: 2, name: "freshwash", image: freshwash, price: "699" },
-  { id: 3, name: "serum", image: serum, price: "899" },
-  { id: 4, name: "medicien", image: medicien, price: "399" },
+  {
+    id: 1,
+    name: "Cream",
+    image: cream,
+    price: "599",
+    description: "Moisturizing cream for soft and glowing skin.",
+    quantity: 50,
+  },
+  {
+    id: 2,
+    name: "Freshwash",
+    image: freshwash,
+    price: "699",
+    description: "Gentle face wash that refreshes and cleanses deeply.",
+    quantity: 70,
+  },
+  {
+    id: 3,
+    name: "Serum",
+    image: serum,
+    price: "899",
+    description: "Lightweight serum that helps brighten and hydrate skin.",
+    quantity: 40,
+  },
+  {
+    id: 4,
+    name: "Medicien",
+    image: medicien,
+    price: "399",
+    description: "Essential medicinal product for daily health needs.",
+    quantity: 100,
+  },
 ];
 
 export default data;
