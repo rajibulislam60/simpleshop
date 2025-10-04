de869e980826f3946738e2e5126bcd7c66d0a75b
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-10-04 23:35:25 +0600

    react router add

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayOut from "./layout/RootLayOut";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route>
+        <Route path="/" element={<RootLayOut />}>
+          <Route index element={<Home />} />
+          {/* <Route path="/shop" element={<Shop />} /> */}
+        </Route>
+      </Route>
+    </>
+  )
+);
 
 const App = () => {
-  return <div className="text-center">Rajib</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
